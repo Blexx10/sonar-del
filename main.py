82b782d6da766e29e61c64a461a765d31f15c4fb
Blexx10 <92892195+Blexx10@users.noreply.github.com>
2021-11-18 11:22:44 -0500

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,55 @@ distance = 0
 def on_forever():
     global distance
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
-    if distance < 10:
-        basic.show_icon(IconNames.NO)
+    if distance < 0:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
     else:
-        basic.show_icon(IconNames.YES)
+        if distance < 2:
+            basic.show_leds("""
+                # . . . .
+                                # . . . .
+                                # . . . .
+                                # . . . .
+                                # . . . .
+            """)
+        else:
+            if distance < 4:
+                basic.show_leds("""
+                    # # . . .
+                                        # # . . .
+                                        # # . . .
+                                        # # . . .
+                                        # # . . .
+                """)
+            if distance < 5:
+                basic.show_leds("""
+                    # # # . .
+                                        # # # . .
+                                        # # # . .
+                                        # # # . .
+                                        # # # . .
+                """)
+            else:
+                if distance < 7:
+                    basic.show_leds("""
+                        # # # # .
+                                                # # # # .
+                                                # # # # .
+                                                # # # # .
+                                                # # # # .
+                    """)
+                if distance < 10:
+                    basic.show_leds("""
+                        # # # # #
+                                                # # # # #
+                                                # # # # #
+                                                # # # # #
+                                                # # # # #
+                    """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,9 +5,53 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    if (distance < 10) {
-        basic.showIcon(IconNames.No)
-    } else {
-        basic.showIcon(IconNames.Yes)
+    if (distance < 50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance < 40 && distance <= 49) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    } else if (distance > 30 && distance <= 40) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+    } else if (distance > 20 && distance <= 30) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+    } else if (distance > 10 && distance <= 20) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+    } else if (distance < 10) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     }
 })
